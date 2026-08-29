72952b153dc5431a4c36372d98bdd8139715a92d
JonasFontess <jonasfontesneto@gmail.com>
2026-08-28 21:34:35 -0300

    Create 11-switch.dart

---
+++ 11-switch.dart
@@ -0,0 +1,12 @@
+void main() {
+  int opcao = 1;
+  switch(opcao){
+      case 1 : print("Cadastrar"); break;
+      case 2 :
+        print("listar: ");
+        break;
+      case 3: print("Sair: "); break;
+      default: 
+        print("opção invalida");  
+  }
+}
