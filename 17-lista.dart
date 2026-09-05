00a547e99e2bab504f9f262d012cee655b3ab21f
Jonas Cardoso Fontes Neto <jonasfontesneto@gmail.com>
2026-09-05 20:41:44 -0300

    Create 17-lista.dart to manage a student list

---
+++ 17-lista.dart
@@ -0,0 +1,9 @@
+void main() {
+  List<String> alunos = ['Aline', 'Beatriz', 'Camila', 'Diego'];
+  print(alunos);
+  alunos.add('Eduardo');
+  print(alunos);
+  alunos.remove('Aline');
+  print(alunos);
+  print(alunos[0]);
+}
